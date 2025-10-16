bcc40f962d955162bfc8749d6a3d8bf48c247a65
Kennysaruni <kennylatimore4@gmail.com>
2025-10-16 22:08:18 +0300

    Moved the commitment section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -22,7 +22,7 @@ function Footer() {
     ];
 
     return (
-        <footer className='bg-background-light dark:bg-background-dark border-t border-gray-200 dark:border-gray-800'>
+        <footer className='bg-background-light dark:bg-background-dark border-t border-gray-200 dark:border-gray-800 mt-5'>
             <div className='container mx-auto px-4 sm:px-6 lg:px-8 py-12'>
                 <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
                     <div className='space-y-4'>
